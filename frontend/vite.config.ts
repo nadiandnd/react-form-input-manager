692f936a2675de5064d50feaf0b0daf77cb90b8a
Nadia <ndtaeminnie@gmail.com>
2025-01-25 21:35:47 +0700

    remove proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,19 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
-  const API_URL = env.VITE_API_URL || "http://localhost:5000";
 
   return {
     plugins: [react()],
-    server: {
-      proxy: {
-        "/api": {
-          target: API_URL,
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ""),
-        },
-      },
-    },
+    server: {},
     define: {
       __APP_ENV__: JSON.stringify(env.APP_ENV),
     },
